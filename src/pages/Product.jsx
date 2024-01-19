9caeda868ab844562f3b084cf03d5c0a6d165eca
Federico Perez <federicoperez@digitalhouse.com>
2024-01-19 16:13:41 -0300

    Created Product page, updated styles and updated mock data file

--- src/pages/Product.jsx
+++ src/pages/Product.jsx
@@ -1,12 +1,64 @@
+import { useEffect, useState } from "react"
 import Footer from "../components/Footer"
 import Header from "../components/Header"
+import Loader from "../components/Loader"
+import products from "../utils/data"
+import { useLocation } from "wouter"
 
 const Product = ({id}) => {
+
+    const [location, setLocation] = useLocation()
+    const [load, setLoad] = useState(true)
+    const [prod, setProd] = useState({})
+
+    if (!id || !location.includes('producto')) {
+        setLocation('/error')
+    }
+
+    const filterProduct = () => {
+        const currProd = products.filter(p => p.id === Number(id))[0]
+        setProd(currProd);
+    }
+
+    useEffect(() => {
+        setTimeout(() => {
+            filterProduct()
+            setLoad(false)
+        }, 2000)
+    }, [])
+
     return (
         <section id="Product" className="flex flex-col h-full">
             <Header/>
             <div className="flex-1 flex flex-col justify-center items-center">
-                <div>Product individual page, id: {id}</div>
+                { !load && (prod && prod.imageSrc) ?
+                    <>      
+                        <img src={prod.imageSrc} alt={prod.imageAlt} className="mx-auto md:h-6/12 md:w-6/12 h-full w-full object-cover object-center"/>
+                        <div className="py-0 px-8 mt-5">
+                            <div className="lg:col-span-2 lg:border-r lg:border-gray-200 lg:pr-8">
+                                <h1 className="text-2xl font-bold tracking-tight text-gray-900 sm:text-3xl">Basic Tee 6-Pack</h1>
+                                <p className="text-3xl tracking-tight text-gray-900">$192</p>
+                            </div>
+                            <div className="mt-10">
+                                <h2 className="text-sm font-medium text-gray-900">Details</h2>
+                                <div className="mt-4 space-y-6">
+                                    <p className="text-sm text-gray-600">The 6-Pack includes two black, two white, and two heather gray Basic Tees. Sign up for our subscription service and be the first to get new, exciting colors, like our upcoming "Charcoal Gray" limited release.</p>
+                                </div>
+                            </div>
+                            <div className="mt-10">
+                                <h3 className="text-sm font-medium text-gray-900">Highlights</h3>
+                                <div className="mt-4">
+                                    <ul role="list" className="list-disc space-y-2 pl-4 text-sm">
+                                        <li className="text-gray-400"><span className="text-gray-600">Hand cut and sewn locally</span></li>
+                                        <li className="text-gray-400"><span className="text-gray-600">Dyed with our proprietary colors</span></li>
+                                        <li className="text-gray-400"><span className="text-gray-600">Pre-washed &amp; pre-shrunk</span></li>
+                                        <li className="text-gray-400"><span className="text-gray-600">Ultra-soft 100% cotton</span></li>
+                                    </ul>
+                                </div>
+                            </div>
+                        </div>
+                    </> : <Loader/>
+                }
             </div>
             <Footer/>
         </section>
---
+++ src/utils/data.js
@@ -0,0 +1,36 @@
+// Mock data
+const products = [
+  {
+    id: 1,
+    name: 'Basic Tee 1',
+    imageSrc: 'https://tailwindui.com/img/ecommerce-images/product-page-01-related-product-01.jpg',
+    imageAlt: "Front of men's Basic Tee in dark grey.",
+    price: '$35',
+    color: 'Dark grey',
+  },
+  {
+    id: 2,
+    name: 'Basic Tee 2',
+    imageSrc: 'https://tailwindui.com/img/ecommerce-images/product-page-01-related-product-01.jpg',
+    imageAlt: "Front of men's Basic Tee in black.",
+    price: '$45',
+    color: 'Black',
+  },
+  {
+    id: 3,
+    name: 'Basic Tee 3',
+    imageSrc: 'https://tailwindui.com/img/ecommerce-images/product-page-01-related-product-01.jpg',
+    imageAlt: "Front of men's Basic Tee in grey.",
+    price: '$25',
+    color: 'Grey',
+  },
+  {
+    id: 4,
+    name: 'Basic Tee 4',
+    imageSrc: 'https://tailwindui.com/img/ecommerce-images/product-page-01-related-product-01.jpg',
+    imageAlt: "Front of men's Basic Tee in light black.",
+    price: '$15',
+    color: 'Light black',
+  }
+]
+export default products
