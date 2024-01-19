297c6f8095d21fc77baddc85fe8df7c5a086c083
Federico Perez <federicoperez@digitalhouse.com>
2024-01-19 12:12:59 -0300

    Deleted unused component, created pages and config routes

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,21 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
 import './index.css'
+import React from 'react'
+import { Switch, Route } from 'wouter'
+import ReactDOM from 'react-dom/client'
+
+// Pages
+import Product from './pages/Product'
+import Products from './pages/Products'
+import NotFound from './pages/NotFound'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <Switch>
+      <Route path="/productos" component={Products}/>
+      <Route path="/producto/:id">
+        {params => <Product id={params.id} />}
+      </Route>
+      <Route component={NotFound}/>
+    </Switch>
   </React.StrictMode>,
 )
---
+++ src/pages/Product.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function Product({id}) {
+  return (
+    <div>Product individual page, id: {id}</div>
+  )
+}
+
+export default Product
