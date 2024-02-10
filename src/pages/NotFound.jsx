01480fb1faad69939e5caea3d28dff0967021432
Federico Perez <federicoperez@digitalhouse.com>
2024-02-10 14:15:49 -0300

    Updated styles and structure for home and notfound pages

--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -7,7 +7,7 @@ const NotFound = () => {
     return (
         <section id="NotFound" className="flex flex-col h-full">
             <Header/>
-            <div className="flex-1 flex flex-col justify-center items-center bg-gradient-to-b from-indigo-200">
+            <div className="flex-1 flex flex-col justify-center items-center bg-gradient-to-b from-blue-100 max-w-screen-lg mx-auto w-full">
                 <h1 className="text-2xl font-sans text-gray-900 sm:text-3xl mb-3 max-w-fit">Ups, ocurrió un error!</h1>
                 <p className="mb-3 max-w-fit text-center">Podés volver al catálogo de productos haciendo click en el botón.</p>
                 <Link href="/productos" type="button" className="rounded-md bg-white px-2.5 py-1.5 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50 flex justify-between align-middle max-w-fit">
